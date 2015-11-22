b0aeb7e60881dfe48aefdbd76f81a615f565adbe
Lindsey Wells <lkwlala@gmail.com>
2015-11-22 22:18:38 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,28 @@
-# Enter your procedural solution here!
+
+def is_prime?(num)
+  if num <= 1
+    false
+  elsif num == 2
+    true
+  elsif (num > 2 && num % 2 != 0)
+    (3..(Math.sqrt(num).to_i)).detect {|n| num % n == 0}.nil?
+  end
+end
+
+def largest_prime_factor(num)
+  factor_pair = nil
+  (3..num).each do |n|
+    if num % n == 0
+      factor_pair = num / n
+      if is_prime?(factor_pair)
+        return factor_pair
+      end
+    end
+  end
+end
+
+
+
+
+
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,35 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(num)
+    @num = num
+  end
+
+  def is_prime? (factor)
+    if factor <= 1
+      false
+    elsif factor == 2
+      true
+    elsif (factor > 2 && factor % 2 != 0)
+      (3..(Math.sqrt(factor).to_i)).detect {|n| factor % n == 0}.nil?
+    end
+  end
+
+  def number
+    factor_pair = nil
+    (3..@num).each do |n|
+      if @num % n == 0
+        factor_pair = @num / n
+        if is_prime?(factor_pair)
+          return factor_pair
+        end
+      end
+    end
+  end
+end
+
+
+
+
+
+
+
+
